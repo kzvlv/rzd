3a73b2d13283b2c051400dae1cac282c3809bc6c
Кирилл <Kirill.kuzovlev2006@gmail.com>
2026-02-20 11:33:29 +0300

    Please enter the commit message for your changes. Lines starting

--- extensions.py
+++ extensions.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-# Мы создаем "пустые" объекты здесь
+# Мы создаем объекты здесь
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
